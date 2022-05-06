299e63ea58bcc1d8273c3bb732226a6e6c65abdf
Jakub Lendzinski <j.lendzinski@outlook.com>
2022-05-06 16:32:59 +0200

    Plant and animal constructors

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -5,6 +5,13 @@ public abstract class Animal implements BoardMovement {
     int endurance;
     int health;
 
+    public Animal(int toReproduce, int endurance, int health){
+        this.timesAteToReproduce = toReproduce;
+        this.ate = 0;
+        this.endurance = endurance;
+        this.health = health;
+    }
+
     public abstract Animal reproduce();
     public abstract boolean Eat(BoardMovement sth);
 }
--- src/main/java/Plant.java
+++ src/main/java/Plant.java
@@ -2,7 +2,12 @@ public abstract class Plant implements BoardMovement{
 
     int roundToReproduce;
     int rounds;
-    int reproduceChange;
+    int reproduceChance;
 
+    public Plant(int toReproduce, int reproduceChance){
+        this.reproduceChance = toReproduce;
+        this.rounds = 0;
+        this.reproduceChance = reproduceChance;
+    }
     public abstract Plant reproduce();
 }
